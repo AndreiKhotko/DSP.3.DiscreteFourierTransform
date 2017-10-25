f6fd12188dd2a7be6577f978c9ded3dcd1dc0a7b
AndreiKhotko <khotko.epam@gmail.com>
2017-10-25 13:44:41 +0300

    add DFT lib

---
+++ DiscreteFourierTransform/Amplitude.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscreteFourierTransform
+{
+    public class Amplitude
+    {
+        public double SinComponent
+        {
+            get
+            {
+                double sum = 0;
+
+                for (int i = 0; i < _signal.N; i++)
+                {
+                    sum += _signal.GetValue(i) * Math.Sin(2 * Math.PI * _signal.J * i / _signal.N);
+                }
+
+                return 2 * sum / _signal.N;
+            }
+        }
+
+        public double CosComponent
+        {
+            get
+            {
+                double sum = 0;
+
+                for (int i = 0; i < _signal.N; i++)
+                {
+                    sum += _signal.GetValue(i) * Math.Cos(2 * Math.PI * _signal.J * i / _signal.N);
+                }
+
+                return 2 * sum / _signal.N;
+            }
+        }
+
+        public double Value => Math.Sqrt(Sqr(SinComponent) + Sqr(CosComponent));
+
+        public double Phase => Math.Atan(SinComponent / CosComponent);
+
+        private readonly Signal _signal;
+        public Amplitude(Signal signal)
+        {
+            if (signal == null)
+                throw new ArgumentNullException(nameof(signal));
+
+            _signal = signal;
+        }
+
+        
+
+        public static double GetPhase()
+        {
+            
+        }
+
+        private static double Sqr(double d) => d * d;
+    }
+}
---
+++ DiscreteFourierTransform/Signal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscreteFourierTransform
+{
+    public class Signal
+    {
+        public Amplitude Amplitude => _amplitude ?? new Amplitude(this);
+
+        public int N
+        {
+            get { return _N; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException($"N = {value} is less or equals to zero");
+
+                _N = value;
+            }
+        }
+
+        public int J
+        {
+            get { return _j; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException($"J = {value} is less than zero");
+
+                _j = value;
+            }
+        }
+
+        private int _N;
+        private int _j;
+        private Amplitude _amplitude;
+        public Signal(int N, int j)
+        {
+            this.N = N;
+            J = j;
+        }
+
+        public double GetValue(int i) => 30 * Math.Cos(2 * Math.PI * i / N - 3 * Math.PI / 4);
+        
+
+    }
+}
